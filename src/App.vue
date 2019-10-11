cb6864b45f6f2d5f637dae0e913f57f7f7b1bdc3
Juan Sebastian Benitez Ospina <jbenitez@hugeinc.com>
2019-10-10 22:25:33 -0500

    Create variables and global scss files

--- src/App.vue
+++ src/App.vue
@@ -9,58 +9,22 @@
 </template>
 
 <script>
-import Navbar from '@/components/Navbar.vue';
-import Footer from '@/components/Footer.vue';
+import "@/sass/_global.scss";
+import "@/sass/_variables.scss";
+import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
   components: {
     Navbar,
-    Footer,
-  },
+    Footer
+  }
 };
 </script>
 
 <style lang="scss">
-* {
-  padding: 0;
-  margin: 0;
-  font-family: 'Noto Sans', sans-serif;
-}
-
-body {
-  background: #2c3e50;
-}
-
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   color: white;
-}
-
-button {
-  background: transparent;
-  border: none;
-  color: inherit;
-  font: inherit;
-}
-
-.container {
-  max-width: 1470px;
-  padding: 30px 0;
-  margin: 0 auto;
-}
-
-.button {
-  background: #ff0000;
-  padding: 10px 15px;
-  border-radius: 10px;
-  cursor: pointer;
-  text-decoration: none;
-  color: white;
-  font-weight: 600;
-  transition: background 0.5s;
-
-  &:hover {
-    background: #AA0000;
-  }
 }
 </style>

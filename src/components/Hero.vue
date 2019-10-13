652afad54e1f6741c3fbbd492cd1141e1b754269
Maria Camila Gomez <camigomez35@gmail.com>
2019-10-12 21:37:35 -0500

    Add parallax to hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -5,15 +5,23 @@
       <h1 class="hero__title">{{title}}</h1>
       <h3 class="hero__subtitle">{{subtitle}}</h3>
     </div>
-
+    <parallax speedFactor="0.3">
+      <img :src="image">
+    </parallax>
   </div>
 </template>
 
 <script>
+import Parallax from 'vue-parallaxy';
+
 export default {
   props: {
     title: String,
     subtitle: String,
+    image: String,
+  },
+  components: {
+    Parallax,
   },
 };
 </script>
@@ -21,7 +29,6 @@ export default {
 <style lang="scss" scoped>
 .hero {
   position: relative;
-  background-image: url("/background.jpeg");
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
